d223bbcfef10960ac00507806aa0fee63e18cfbf
sakisaki <189444236@qq.com>
2025-10-14 22:34:05 +0800

    feat: initialize full-stack project structure
    - Add frontend Vue application
    - Add backend SpringBoot framework

---
+++ saki-ai-code-tools-backend/src/main/java/com/saki/sakiaicodetoolsbackend/SakiAiCodeToolsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saki.sakiaicodetoolsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakiAiCodeToolsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakiAiCodeToolsBackendApplication.class, args);
+    }
+
+}
