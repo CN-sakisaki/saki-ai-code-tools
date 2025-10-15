ddda17099e1627c146c2d9ab78013eefee091298
sakisaki <189444236@qq.com>
2025-10-15 15:11:01 +0800

    feat：添加AOP依赖并在启动类中加入注解

--- saki-ai-code-tools-backend/src/main/java/com/saki/sakiaicodetoolsbackend/SakiAiCodeToolsBackendApplication.java
+++ saki-ai-code-tools-backend/src/main/java/com/saki/sakiaicodetoolsbackend/SakiAiCodeToolsBackendApplication.java
@@ -2,8 +2,10 @@ package com.saki.sakiaicodetoolsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class SakiAiCodeToolsBackendApplication {
 
     public static void main(String[] args) {
