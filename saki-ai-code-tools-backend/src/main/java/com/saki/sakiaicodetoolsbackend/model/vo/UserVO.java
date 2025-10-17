29435b64a01081d0bac22573bc676ecfc3d97bad
sakisaki <189444236@qq.com>
2025-10-17 21:55:40 +0800

    feat：添加登录认证相关常量、DTO、enums、vo等类；
    - 添加获取获取IP的工具类
    - yml中添加jwt相关的配置

--- saki-ai-code-tools-backend/src/main/java/com/saki/sakiaicodetoolsbackend/model/vo/UserVO.java
+++ saki-ai-code-tools-backend/src/main/java/com/saki/sakiaicodetoolsbackend/model/vo/UserVO.java
@@ -1,19 +1,27 @@
 package com.saki.sakiaicodetoolsbackend.model.vo;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import com.saki.sakiaicodetoolsbackend.model.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
  * 用户登录成功后返回给前端的视图对象。
+ * @author saki酱
+ * @version 1.0
+ * @since 2025-10-17 11:39
  */
 @Data
 @Schema(description = "用户信息视图对象")
 public class UserVO implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    @Serial
+    private static final long serialVersionUID = -989032212459964155L;
 
     private Long id;
 
@@ -48,5 +56,10 @@ public class UserVO implements Serializable {
     private LocalDateTime createTime;
 
     private String accessToken;
+
+    public void copyUserInfoFrom(User user) {
+        BeanUtil.copyProperties(user, this, CopyOptions.create().ignoreNullValue());
+    }
+
 }
 
