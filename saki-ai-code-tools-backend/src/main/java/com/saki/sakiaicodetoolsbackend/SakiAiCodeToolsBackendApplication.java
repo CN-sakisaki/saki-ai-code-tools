353674c0395922d3cae604fabe201fb048179ea4
sakisaki <189444236@qq.com>
2025-10-26 23:04:40 +0800

    refactor：
    - 删除权限校验AOP中的冗余代码；
    - 添加 JSON 配置类解决 Long 类型在前端精度丢失问题；
    - 删除启动类中检查 mapper 是否注册的代码

--- saki-ai-code-tools-backend/src/main/java/com/saki/sakiaicodetoolsbackend/SakiAiCodeToolsBackendApplication.java
+++ saki-ai-code-tools-backend/src/main/java/com/saki/sakiaicodetoolsbackend/SakiAiCodeToolsBackendApplication.java
@@ -1,14 +1,9 @@
 package com.saki.sakiaicodetoolsbackend;
 
-import jakarta.annotation.PostConstruct;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-import java.util.Arrays;
 
 
 /**
@@ -21,16 +16,8 @@ import java.util.Arrays;
 @EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan("com.saki.sakiaicodetoolsbackend.mapper")
 public class SakiAiCodeToolsBackendApplication {
-    @Autowired
-    private ApplicationContext applicationContext;
 
-    @PostConstruct
-    public void checkMapper() {
-        String[] mappers = applicationContext.getBeanNamesForType(com.saki.sakiaicodetoolsbackend.mapper.UserMapper.class);
-        System.out.println("UserMapper beans = " + Arrays.toString(mappers));
-    }
     public static void main(String[] args) {
-
         SpringApplication.run(SakiAiCodeToolsBackendApplication.class, args);
     }
 
